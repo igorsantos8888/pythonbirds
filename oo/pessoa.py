f550660c47f3d5a0a45ff0f26e4c1bb78b9c882a
Igor Santos <igorsantos888@gmail.com>
2022-07-19 10:05:50 -0300

    criados os atributos de instancia nome, idade e localidade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,18 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=None, localidade=None):
+        self.nome = nome
+        self.idade = idade
+        self.localidade = localidade
+    
+    
     def cumprimentar(self):
         return f'Olá {id(self)}'
     
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Igor', 25, 'RJ')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
+    print(p.localidade)
